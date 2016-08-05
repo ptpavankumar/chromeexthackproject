bfb91e5b093fe39040c6f12a0c4ab681c767f2ae
Pavan Kumar <ptpavankumar@hotmail.com>
2016-08-05 10:58:11 +1000

    Code cleanup

--- popup.js
+++ popup.js
@@ -1,29 +1,4 @@
-
-function click(e) {
-  if (e.target.id === 'reset') {
-    chrome.tabs.executeScript(null,
-        {code:"document.body.style.backgroundColor='white'"});
-  } else {
-    chrome.tabs.executeScript(null,
-        {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
-  }
-
-  //window.close();
-}
-
 console.log('Welcome to myob plugin!');
-
-$.ajax({
-  type: "GET",
-  url: "https://qa4-practice.addevcloudservices.com.au/healthcheck",
-  //data: data,
-  success: function(msg){
-    console.log(JSON.stringify(msg));
-  },
-  error: function(){
-    console.log("Error occurred!");
-  }
-});
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -36,28 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   $( "#portalUrl" ).click(function() {
-    //console.log('url>>>>>>>', $("#portalUrl").href);
     chrome.tabs.create({url: $("#portalUrl")[0].href},function(tab) {
       console.log('tab id', tab);
     });
   });
 
-  // var checkPageButton = document.getElementById('checkPage');
-  // checkPageButton.addEventListener('click', function() {
-  //
-  //   chrome.tabs.getSelected(null, function(tab) {
-  //     d = document;
-  //
-  //     var f = d.createElement('form');
-  //     f.action = 'https://qa4-practice.addevcloudservices.com.au/healthcheck';
-  //     f.method = 'get';
-  //     var i = d.createElement('input');
-  //     i.type = 'hidden';
-  //     i.name = 'url';
-  //     i.value = tab.url;
-  //     f.appendChild(i);
-  //     d.body.appendChild(f);
-  //     f.submit();
-  //   });
-  // }, false);
+  $( "#createTaskUIUrl" ).click(function() {
+    chrome.tabs.create({url: $("#portalUrl")[0].href},function(tab) {
+      console.log('tab id', tab);
+    });
+  });
+
 });
