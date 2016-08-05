af93f27cc4b47116906bf72152f66586c7b551cd
Pavan Kumar <ptpavankumar@hotmail.com>
2016-08-05 14:13:49 +1000

    Added additional functionality

--- js/window.js
+++ js/window.js
@@ -1,13 +1,28 @@
 chrome.runtime.onMessage.addListener(
-  function(req, sender, sendResponse) {
-    console.log(req);
-  if (req.details) {
-    var details = req.details;
-    $('input[name="title"]').val(details.title);
-    $('input[name="url"]').val(details.url);
-    $('input[name="page_url"]').val(details.pageUrl);
-    $('input[name="id"]').val(details.id);
-    $('input[name="anchor"]').val(details.text);
-    $('input[name="editable"]').val(details.editable);
-  }
-});
+    function (req, sender, sendResponse) {
+
+        if (req.action == 'alert') {
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                chrome.tabs.captureVisibleTab(null, {}, function (image) {
+                    alert('Image captured!');
+                });
+            });
+        }
+
+        if (req.action == 'browse') {
+            console.log(document);
+            var myForm = document.createElement("FORM");
+            var myFile = document.createElement("INPUT");
+            myFile.type = "file";
+            myFile.id = "selectFile";
+            myFile.onclick = "openDialog()";
+            myForm.appendChild(myFile);
+            var myButton = document.createElement("INPUT");
+            myButton.name = "submit";
+            myButton.type = "submit";
+            myButton.value = "Submit";
+            myForm.appendChild(myButton);
+            document.body.appendChild(myForm);
+        };
+    }
+);
--- popup.js
+++ popup.js
@@ -2,24 +2,42 @@ console.log('Welcome to myob plugin!');
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  // var divs = document.querySelectorAll('div');
-  // for (var i = 0; i < divs.length; i++) {
-  //   divs[i].addEventListener('click', click);
-  // }
-  $( "#partner" ).click(function() {
-    alert( "healthcheck called!" );
-  });
 
-  $( "#portalUrl" ).click(function() {
-    chrome.tabs.create({url: $("#portalUrl")[0].href},function(tab) {
-      console.log('tab id', tab);
+    $("#openPracticeOnline").click(function () {
+        chrome.tabs.create({url: "http://partner.myob.com"}, function (tab) {
+            console.log('tab id', tab);
+        });
     });
-  });
 
-  $( "#createTaskUIUrl" ).click(function() {
-    chrome.tabs.create({url: $("#portalUrl")[0].href},function(tab) {
-      console.log('tab id', tab);
+    $("#createTask").click(function () {
+        chrome.windows.create({
+            type: "panel",
+            url: 'createTask.html',
+            left: 1500,
+            width: 400,
+            height: 1200
+        }, function () {
+            console.log('Im in called after creation of popup!');
+
+
+        });
     });
-  });
 
+    $("#takeSnapshot").click(function () {
+        chrome.runtime.sendMessage({action: "alert", text: "STOP!"}, function(response) {
+            console.log('message sent!')
+        });
+    });
+
+    $("#uploadFile").click(function () {
+
+        chrome.runtime.sendMessage({
+            action: 'browse'
+        });
+        // chrome.tabs.create({url: "http://partner.myob.com" },function(tab) {
+        //   chrome.runtime.sendMessage({
+        //     action: 'browse'
+        //   });
+        // });
+    });
 });
