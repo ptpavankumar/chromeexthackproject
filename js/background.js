25c16de72a16d62bc66f5061beef30503c515272
Pavan Kumar <ptpavankumar@hotmail.com>
2016-08-05 11:59:45 +1000

    Added side popup when selected

---
+++ js/background.js
@@ -0,0 +1,24 @@
+chrome.contextMenus.create({
+    'title': 'Get Metadata',
+    'contexts': ['link'],
+    'onclick': onClickHandler
+});
+
+function onClickHandler(info) {
+    var details = {}
+
+    details.title = info.selectionText;
+    details.url = info.linkUrl;
+    details.editable = info.editable;
+    details.id = info.menuItemId;
+    details.pageUrl = info.pageUrl;
+    details.text = info.selectionText;
+
+    chrome.windows.create({ url: 'window.html', type: 'popup', left: 1500, top: 300, width: 400, height: 1200 }, function() {
+        console.log('Im in called after creation of popup!');
+
+        chrome.runtime.sendMessage({ details: details }, function(response) {
+            alert('Im in called after creation of popup!');
+        });
+    });
+}
